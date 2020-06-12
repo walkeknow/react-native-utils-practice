aff5bf6abf5063301b1a31f56350852b3f48982f
walkeknow <kewalshah077@gmail.com>
2020-06-12 18:48:47 +0530

    Add image from Library

--- App.js
+++ App.js
@@ -1,12 +1,5 @@
 import React, { Fragment, Component } from 'react'
-import {
-  StyleSheet,
-  Text,
-  View,
-  Platform,
-  Settings,
-  Animated,
-} from 'react-native'
+import { StyleSheet, Text, View, Platform, Animated, Image } from 'react-native'
 import styled from 'styled-components'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { NavigationContainer } from '@react-navigation/native'
@@ -14,8 +7,11 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createDrawerNavigator } from '@react-navigation/drawer'
+import ImageEditor from '@react-native-community/image-editor'
+import * as ImagePicker from 'expo-image-picker'
+import Constants from 'expo-constants'
 
-// Nesting order Top to Bottom:
+// Navigation nesting order Top to Bottom:
 
 // Drawer Navigator => Opens TabNavigator (Home) and System Settings
 
@@ -110,18 +106,50 @@ function StackNavigator() {
   )
 }
 
-function Home({ navigation }) {
-  return (
-    <CenterView>
-      <WelcomeText>Hello, World!</WelcomeText>
-      <WelcomeBtn onPress={() => navigation.navigate('AppSettings')}>
-        <WelcomeText>App Settings</WelcomeText>
-      </WelcomeBtn>
-      <WelcomeBtn onPress={() => navigation.openDrawer()}>
-        <WelcomeText>Drawer Navigator </WelcomeText>
-      </WelcomeBtn>
-    </CenterView>
-  )
+class Home extends Component {
+  state = {
+    image: null,
+  }
+  componentDidMount() {
+    if (Constants.platform.ios) {
+      ImagePicker.requestCameraRollPermissionsAsync().then(({ status }) => {
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!')
+        }
+      })
+    }
+  }
+  pickImage = () => {
+    ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [2, 1],
+    }).then((result) => {
+      if (result.cancelled) {
+        return
+      }
+
+      return this.setState(() => ({ image: result.uri }))
+    })
+  }
+  render() {
+    const { navigation } = this.props
+    const { image } = this.state
+    return (
+      <CenterView>
+        <WelcomeText>Hello, World!</WelcomeText>
+        <WelcomeBtn onPress={() => navigation.navigate('AppSettings')}>
+          <WelcomeText>App Settings</WelcomeText>
+        </WelcomeBtn>
+        <WelcomeBtn onPress={() => navigation.openDrawer()}>
+          <WelcomeText>Drawer Navigator </WelcomeText>
+        </WelcomeBtn>
+        <WelcomeBtn onPress={this.pickImage}>
+          <WelcomeText>Open Gallery</WelcomeText>
+        </WelcomeBtn>
+        {image && <Image style={styles.img} source={{ uri: image }} />}
+      </CenterView>
+    )
+  }
 }
 
 class Dashboard extends Component {
@@ -134,8 +162,8 @@ class Dashboard extends Component {
     const { opacity, width, height } = this.state
 
     Animated.timing(opacity, { toValue: 1, duration: 1000 }).start()
-    Animated.spring(width, {toValue: 300, speed: 5}).start()
-    Animated.spring(height, {toValue: 300, speed: 5}).start()
+    Animated.spring(width, { toValue: 300, speed: 5 }).start()
+    Animated.spring(height, { toValue: 300, speed: 5 }).start()
   }
   render() {
     const { opacity, width, height } = this.state
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-## Using React Navigation, Animation and Styled Components
+## Using Navigation, Animation, ImagePicker and Styled Components in React Native Apps
 
 Features
 
 - Includes a basic example showing how to use and nest Tab, Drawer, and Stack Navigators using the Expo react-navigation package
 - Shows how to style Components using the styled-components library
 - Includes an example of a scaling and fading-in animation using the built-in Animation library
+- Shows how to use Expo's ImagePicker library to select and display an image
